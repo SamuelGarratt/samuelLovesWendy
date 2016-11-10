9047d73cfe40b0346cc32e03f1d52986bc9f9f4c
Samuel Garratt <samuel.garratt@integrationqa.com>
2016-11-11 06:57:39 +1300

    Playing around with timer

--- js/countdown.js
+++ js/countdown.js
@@ -1,19 +1,25 @@
 function startTimer(duration, display) {
+    var hasTimed = false;
     var timer = duration, minutes, seconds;
     setInterval(function () {
         minutes = parseInt(timer / 60, 10);
         seconds = parseInt(timer % 60, 10);
 
-        minutes = minutes < 10 ? "0" + minutes : minutes;
-        seconds = seconds < 10 ? "0" + seconds : seconds;
+        minutes = minutes < 10 ? + minutes : minutes;
+        seconds = seconds < 10 ? + seconds : seconds;
 
 		if (seconds < 65)
 			display.textContent = seconds + (minutes * 60);
 		else
 			display.textContent = minutes + ":" + seconds;
 
-        if (--timer < 0) {
-            timer = duration;
+        if (!hasTimed)
+            {
+            if (--timer < 0) {            
+                timer = duration;
+                hasTimed = true;
+                }            
+            }
         }
     }, 1000);
 }
