9f82338afee8bd80ccd9eb43f6c1a35789f370f8
Samuel Garratt <samuel.garratt@integrationqa.com>
2016-11-23 06:48:39 +1300

    Clean up timer with better implementation

--- js/countdown.js
+++ js/countdown.js
@@ -1,26 +1,40 @@
-function startTimer(duration, display) {
-    var hasTimed = false;
-    var timer = duration, minutes, seconds;
-    setInterval(function () {
-        minutes = parseInt(timer / 60, 10);
-        seconds = parseInt(timer % 60, 10);
-
-        minutes = minutes < 10 ? + minutes : minutes;
-        seconds = seconds < 10 ? + seconds : seconds;
-
-		if (seconds < 65)
-			display.textContent = seconds + (minutes * 60);
-		else
-			display.textContent = minutes + ":" + seconds;
-
-        if (!hasTimed)
-            {
-            if (--timer < 0) {            
-                timer = duration;
-                hasTimed = true;
-                }            
-            }
-        }
-    }, 1000);
+function getTimeRemaining(endtime) {
+  var t = Date.parse(endtime) - Date.parse(new Date());
+  var seconds = Math.floor((t / 1000) % 60);
+  var minutes = Math.floor((t / 1000 / 60) % 60);
+  var hours = Math.floor((t / (1000 * 60 * 60)) % 24);
+  var days = Math.floor(t / (1000 * 60 * 60 * 24));
+  return {
+    'total': t,
+    'days': days,
+    'hours': hours,
+    'minutes': minutes,
+    'seconds': seconds
+  };
 }
 
+function initializeClock(id, endtime) {
+  var clock = document.getElementById(id);  
+  var minutesSpan = clock.querySelector('.minutes');
+  var secondsSpan = clock.querySelector('.seconds');
+
+  function updateClock() {
+    var t = getTimeRemaining(endtime);
+    
+    minutesSpan.innerHTML = ('0' + t.minutes).slice(-2);
+    secondsSpan.innerHTML = ('0' + t.seconds).slice(-2);
+
+    if (t.total <= 0) {
+      clearInterval(timeinterval);
+    }
+  }
+
+  updateClock();
+  var timeinterval = setInterval(updateClock, 1000);
+}
+
+
+var currentTime = Date.parse(new Date());
+var deadline = new Date(currentTime + timeInSeconds*1000);
+
+initializeClock('clockdiv', deadline);
